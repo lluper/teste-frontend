61c3f2eca927949f0923ab72d35e69119a65b812
lluper <leonardoluper5@gmail.com>
2020-11-06 08:36:34 -0300

    Add constants in main

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "@/plugins/vuetify";
+import "./assets/global.css";
 Vue.config.productionTip = false;
 
+Vue.prototype.$appName = "Teste front Leonardo Luper";
+Vue.prototype.$API_KEY = "AIzaSyCPMk0E9GN1e9pqLXOsMcP1z82iO13EzWw";
+
+localStorage.clear();
 new Vue({
   router,
   vuetify,
